bec530b1a9b3d79201a7e55cd93add32f8d82ea6
cheasles <cheasles@github.com>
2020-12-22 22:21:12 +1300

    Added empty google test module.

---
+++ test/common/src/Common/log.cpp
@@ -0,0 +1,10 @@
+#include <common/log.h>
+
+#include "gtest/gtest.h"
+
+TEST(Log, GetLabel) {
+  // This test is named "Negative", and belongs to the "FactorialTest"
+  // test case.
+  EXPECT_EQ("DEBUG", openrs::common::Log::GetLabel(
+                         openrs::common::Log::LogLevel::kDebug));
+}
