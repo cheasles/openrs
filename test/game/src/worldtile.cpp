f0566726e82f949ed7f18b0b5a0f9db96e7a6792
cheasles <cheasles@github.com>
2020-12-23 16:07:09 +1300

    Added WorldTile mutator test.

--- test/game/src/worldtile.cpp
+++ test/game/src/worldtile.cpp
@@ -9,3 +9,16 @@ TEST(WorldTile, Accessors) {
   EXPECT_EQ(0, world_tile.z());
   EXPECT_EQ(world_tile.z(), world_tile.plane());
 }
+
+TEST(WorldTile, Mutators) {
+  openrs::game::WorldTile world_tile;
+  world_tile.set_x(5);
+  world_tile.set_y(6);
+  world_tile.set_z(7);
+  EXPECT_EQ(5, world_tile.x());
+  EXPECT_EQ(6, world_tile.y());
+  EXPECT_EQ(7, world_tile.z());
+
+  world_tile.set_plane(8);
+  EXPECT_EQ(world_tile.z(), world_tile.plane());
+}
