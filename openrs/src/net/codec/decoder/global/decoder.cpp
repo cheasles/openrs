bdcfcd59fbf25585ba4cd471d57f6be049bfbabe
cheasles <cheasles@github.com>
2021-01-08 22:21:42 +1300

    Added license header.

--- openrs/src/net/codec/decoder/global/decoder.cpp
+++ openrs/src/net/codec/decoder/global/decoder.cpp
@@ -1,3 +1,19 @@
+/**
+ * This file is part of OpenRS.
+ *
+ * OpenRS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * OpenRS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenRS. If not, see <https://www.gnu.org/licenses/>.
+ */
 #include "openrs/net/codec/decoder/global/decoder.h"
 
 #include <openrs/common/log.h>
